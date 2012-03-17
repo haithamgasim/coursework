85a9fcfc6615979e2e4fe00ca1cd51fbe17881a3
Haitham Gasim <haitham.gasim@gmail.com>
2012-03-17 12:23:09 -0500

    Committing my week 1 exercises

---
+++ p2pu/intro_to_ruby/ch4_fav_num_exercise.rb
@@ -0,0 +1,4 @@
+puts 'What\'s your favorite number?'
+input = gets.chomp
+result = input.to_i + 1
+puts 'Well ' + result.to_s + ' is always a bigger and better favorite number'
---
+++ p2pu/intro_to_ruby/ch4_greeting_exercise.rb
@@ -0,0 +1,7 @@
+puts 'What\'s your first name?'
+fname = gets.chomp
+puts 'What\'s your middle Initial?'
+mi = gets.chomp
+puts 'What\'s your last name?'
+lname = gets.chomp
+puts 'Welcome ' + fname + ' ' + mi + '. ' + lname + '!'
